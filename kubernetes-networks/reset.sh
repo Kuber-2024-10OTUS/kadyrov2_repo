c5d841246a2cf8a79d8244c7185fb0be301b7c42
kadyrov2 <timurlimur7@gmail.com>
2024-11-28 22:03:20 +0300

    dz

---
+++ kubernetes-networks/reset.sh
@@ -0,0 +1,6 @@
+kubectl delete -f ./service.yaml
+kubectl delete -f ./deployment.yaml
+kubectl delete -f ./ingress.yaml
+kubectl apply -f ./service.yaml
+kubectl apply -f ./deployment.yaml
+kubectl apply -f ./ingress.yaml

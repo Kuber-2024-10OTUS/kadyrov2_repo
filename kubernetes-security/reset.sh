2504e00b23598384b20737d0ea2d4e2d14fa7e76
kadyrov2 <timurlimur7@gmail.com>
2025-01-22 14:57:24 +0300

    dz1

--- kubernetes-security/reset.sh
+++
@@ -1,3 +0,0 @@
-kubectl delete    -f ./pvc.yaml    -f ./deployment.yaml  -f ./svc.yaml   -f ./ingress.yaml   -f ./nginx-config.yaml     -f ./config-text-file.yaml   -f ./monitoring-sa.yaml -f ./monitoring-cr.yaml -f ./monitoring-crb.yaml -f ./cd-sa.yaml   -f ./cd-rb.yaml  
-
-kubectl apply     -f ./pvc.yaml   -f ./deployment.yaml  -f ./svc.yaml    -f ./ingress.yaml   -f ./nginx-config.yaml    -f ./config-text-file.yaml   -f ./monitoring-sa.yaml -f ./monitoring-cr.yaml -f ./monitoring-crb.yaml   -f ./cd-sa.yaml   -f ./cd-rb.yaml  
